2b04bf6ce5c0bb912b1d25dad4ae90ba4a0d29c5
Pures8098 <Pures8098@HRH-IC0027324.wrdsb.ca>
2017-09-28 12:52:00 -0400

    finnished Asignments 1-5 of assingment 1

---
+++ src/assingment_3/A3Q1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assingment_3;
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
+/**
+ *
+ * @author Pures8098
+ */
+public class A3Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //create city
+         //Create city
+        City Owensville = new City();
+        
+        //create robot
+        Robot OWEN = new Robot(Owensville, 1, 1, Direction.EAST);
+        
+        //create and move mounting variable
+        
+        
+    }
+}
