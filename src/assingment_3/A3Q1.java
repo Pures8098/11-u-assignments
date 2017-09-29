ebaa7da6547fa5041e12019d94da7d1b2d4cf741
Pures8098 <Pures8098@HRH-IC0027324.wrdsb.ca>
2017-09-29 13:44:41 -0400

    finnished Asignments 1-5 of assingment 1

--- src/assingment_3/A3Q1.java
+++ src/assingment_3/A3Q1.java
@@ -20,13 +20,13 @@ public class A3Q1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         //create city
         City Owensville = new City();
-        
+
         //create robot
-        Robot OWEN = new Robot(Owensville, 0, 2, Direction.WEST); 
-        
+        Robot OWEN = new Robot(Owensville, 0, 3, Direction.WEST);
+
         //create wall
         new Wall(Owensville, 1, 2, Direction.NORTH);
         new Wall(Owensville, 1, 1, Direction.NORTH);
@@ -36,18 +36,28 @@ public class A3Q1 {
         new Wall(Owensville, 2, 1, Direction.WEST);
         new Wall(Owensville, 1, 2, Direction.EAST);
         new Wall(Owensville, 2, 2, Direction.EAST);
-        
+
         //move robot
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        //create and move mounting variable
+
+        for (int count = 0; count < 8; count = count + 1) {
+
+            int moveCounter = 0;
+
+            //continue whe counter is less than ten
+            while (moveCounter < 3) {
+                //get OWEN to move
+                OWEN.move();
+                //add one to move counter
+                moveCounter = moveCounter + 1;
+
+            }
+            OWEN.turnLeft();
+
+        }
+
+
+
+
     }
 }
