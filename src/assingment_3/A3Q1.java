d893628b544f672bd7ebe9d483ad67ae89554c00
Pures8098 <Pures8098@HRH-IC0027324.wrdsb.ca>
2017-09-29 13:35:46 -0400

    finnished Asignments 1-5 of assingment 1

--- src/assingment_3/A3Q1.java
+++ src/assingment_3/A3Q1.java
@@ -3,11 +3,12 @@
  * and open the template in the editor.
  */
 package assingment_3;
+
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
-import becker.robots.Thing;
 import becker.robots.Wall;
+
 /**
  *
  * @author Pures8098
@@ -21,13 +22,31 @@ public class A3Q1 {
         // TODO code application logic here
         
         //create city
-         //Create city
         City Owensville = new City();
         
         //create robot
-        Robot OWEN = new Robot(Owensville, 1, 1, Direction.EAST);
+        Robot OWEN = new Robot(Owensville, 0, 2, Direction.WEST); 
         
-        //create and move mounting variable
+        //create wall
+        new Wall(Owensville, 1, 2, Direction.NORTH);
+        new Wall(Owensville, 1, 1, Direction.NORTH);
+        new Wall(Owensville, 2, 2, Direction.SOUTH);
+        new Wall(Owensville, 2, 1, Direction.SOUTH);
+        new Wall(Owensville, 1, 1, Direction.WEST);
+        new Wall(Owensville, 2, 1, Direction.WEST);
+        new Wall(Owensville, 1, 2, Direction.EAST);
+        new Wall(Owensville, 2, 2, Direction.EAST);
+        
+        //move robot
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
     }
