8ab645b5bde30404e0951adc6f8b7b11e2a331e4
Pures8098 <Pures8098@HRH-IC0027324.wrdsb.ca>
2017-10-02 12:41:17 -0400

    finnished Asignments 1-5 of assingment 1

---
+++ src/assingment_3/A3Q3.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assingment_3;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+
+/**
+ *
+ * @author Pures8098
+ */
+public class A3Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        //Create city
+        City Owensville = new City();
+
+        //create robot
+        Robot OWEN = new Robot(Owensville, 1, 1, Direction.EAST, 20);
+
+        Owensville.showThingCounts(true);
+
+        //move robot
+            for (int count = 0; count < 4; count = count + 1) {
+                int moveCounter = 0;
+
+                //continue whe counter is less than ten
+                while (moveCounter < 5) {
+                    //get OWEN to move
+                    OWEN.putThing();
+                    OWEN.move();
+                    //add one to move counter
+                    moveCounter = moveCounter + 1;
+                }
+                if (OWEN.getDirection() == Direction.EAST) {
+                    if (OWEN.getAvenue() == 6) {
+
+                        OWEN.turnLeft();
+                        OWEN.turnLeft();
+                        OWEN.turnLeft();
+                        OWEN.move();
+                        OWEN.turnLeft();
+                        OWEN.turnLeft();
+                        OWEN.turnLeft();
+                        OWEN.move();
+                    }
+                }
+
+                if (OWEN.getDirection() == Direction.WEST) {
+                    if (OWEN.getAvenue() == 0) {
+                        OWEN.turnLeft();
+                        OWEN.move();
+                        OWEN.turnLeft();
+                        OWEN.move();
+
+
+                    }
+                }
+            }
+        }
+    }
+
