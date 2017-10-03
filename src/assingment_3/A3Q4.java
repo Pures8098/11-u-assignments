1ef32217e84f8c78344ebd7bac60a4b86f112698
Pures8098 <Pures8098@HRH-IC0027324.wrdsb.ca>
2017-10-03 13:36:06 -0400

    finnished Asignments 1-5 of assingment 1

--- src/assingment_3/A3Q4.java
+++ src/assingment_3/A3Q4.java
@@ -21,47 +21,47 @@ public class A3Q4 {
     public static void main(String[] args) {
         // TODO code application logic here
         //create city
-        City Owensville = new City();
+        City owensville = new City();
 
         //Create Robot
-        Robot OWEN = new Robot(Owensville, 3, 3, Direction.SOUTH);
+        Robot oWEN = new Robot(owensville, 3, 3, Direction.SOUTH);
 
         //create wall
-        new Wall(Owensville, 1, 1, Direction.WEST);
-        new Wall(Owensville, 2, 1, Direction.WEST);
-        new Wall(Owensville, 1, 1, Direction.NORTH);
-        new Wall(Owensville, 1, 2, Direction.NORTH);
-        new Wall(Owensville, 2, 1, Direction.SOUTH);
-        new Wall(Owensville, 2, 2, Direction.SOUTH);
-        new Wall(Owensville, 2, 2, Direction.EAST);
-        new Wall(Owensville, 1, 2, Direction.EAST);
-        new Wall(Owensville, 1, 4, Direction.WEST);
-        new Wall(Owensville, 2, 4, Direction.WEST);
-        new Wall(Owensville, 1, 4, Direction.NORTH);
-        new Wall(Owensville, 1, 5, Direction.NORTH);
-        new Wall(Owensville, 2, 4, Direction.SOUTH);
-        new Wall(Owensville, 2, 5, Direction.SOUTH);
-        new Wall(Owensville, 2, 5, Direction.EAST);
-        new Wall(Owensville, 1, 5, Direction.EAST);
-        new Wall(Owensville, 4, 1, Direction.WEST);
-        new Wall(Owensville, 5, 1, Direction.WEST);
-        new Wall(Owensville, 4, 1, Direction.NORTH);
-        new Wall(Owensville, 4, 2, Direction.NORTH);
-        new Wall(Owensville, 5, 1, Direction.SOUTH);
-        new Wall(Owensville, 5, 2, Direction.SOUTH);
-        new Wall(Owensville, 5, 2, Direction.EAST);
-        new Wall(Owensville, 4, 2, Direction.EAST);
-        new Wall(Owensville, 4, 4, Direction.WEST);
-        new Wall(Owensville, 5, 4, Direction.WEST);
-        new Wall(Owensville, 4, 4, Direction.NORTH);
-        new Wall(Owensville, 4, 5, Direction.NORTH);
-        new Wall(Owensville, 5, 4, Direction.SOUTH);
-        new Wall(Owensville, 5, 5, Direction.SOUTH);
-        new Wall(Owensville, 5, 5, Direction.EAST);
-        new Wall(Owensville, 4, 5, Direction.EAST);
+        new Wall(owensville, 1, 1, Direction.WEST);
+        new Wall(owensville, 2, 1, Direction.WEST);
+        new Wall(owensville, 1, 1, Direction.NORTH);
+        new Wall(owensville, 1, 2, Direction.NORTH);
+        new Wall(owensville, 2, 1, Direction.SOUTH);
+        new Wall(owensville, 2, 2, Direction.SOUTH);
+        new Wall(owensville, 2, 2, Direction.EAST);
+        new Wall(owensville, 1, 2, Direction.EAST);
+        new Wall(owensville, 1, 4, Direction.WEST);
+        new Wall(owensville, 2, 4, Direction.WEST);
+        new Wall(owensville, 1, 4, Direction.NORTH);
+        new Wall(owensville, 1, 5, Direction.NORTH);
+        new Wall(owensville, 2, 4, Direction.SOUTH);
+        new Wall(owensville, 2, 5, Direction.SOUTH);
+        new Wall(owensville, 2, 5, Direction.EAST);
+        new Wall(owensville, 1, 5, Direction.EAST);
+        new Wall(owensville, 4, 1, Direction.WEST);
+        new Wall(owensville, 5, 1, Direction.WEST);
+        new Wall(owensville, 4, 1, Direction.NORTH);
+        new Wall(owensville, 4, 2, Direction.NORTH);
+        new Wall(owensville, 5, 1, Direction.SOUTH);
+        new Wall(owensville, 5, 2, Direction.SOUTH);
+        new Wall(owensville, 5, 2, Direction.EAST);
+        new Wall(owensville, 4, 2, Direction.EAST);
+        new Wall(owensville, 4, 4, Direction.WEST);
+        new Wall(owensville, 5, 4, Direction.WEST);
+        new Wall(owensville, 4, 4, Direction.NORTH);
+        new Wall(owensville, 4, 5, Direction.NORTH);
+        new Wall(owensville, 5, 4, Direction.SOUTH);
+        new Wall(owensville, 5, 5, Direction.SOUTH);
+        new Wall(owensville, 5, 5, Direction.EAST);
+        new Wall(owensville, 4, 5, Direction.EAST);
 
         //move robot
-       for (int Sunsar = 0; Sunsar < 4; Sunsar = Sunsar + 1) 
+       for (int sunsar = 0; sunsar < 4; sunsar = sunsar + 1) 
            
        {
 
@@ -70,17 +70,19 @@ public class A3Q4 {
 
                 //continue whe counter is less than 3
                 while (moveCounter < 3) {
-                    OWEN.move();
+                    oWEN.move();
 
                     //add one to move counter
                     moveCounter = moveCounter + 1;
                 }
-                OWEN.turnLeft();
+                //turn corner
+                oWEN.turnLeft();
 
             }
-            OWEN.move();
-            OWEN.move();
-            OWEN.move();
+            //robot moves through open area
+            oWEN.move();
+            oWEN.move();
+            oWEN.move();
         }
     }
 }
