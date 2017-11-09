f9f805765dbb667c90a625821ea96e8ac69f1114
Pures8098 <Pures8098@HRH-IC0027324.wrdsb.ca>
2017-11-09 13:33:26 -0500

    finnished Asignments 1-5 of assingment 1

--- src/assignment_5/A5Q1.java
+++ src/assignment_5/A5Q1.java
@@ -17,13 +17,13 @@ public class A5Q1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-       //start at 0
+        //start at 0
         int a = 0;
         //while the word is greater than zero
-        while(a < 1){
-        
-         System.out.print("Please enter your word: ");
-        //get word
+        while (a < 1) {
+
+            System.out.print("Please enter your word: ");
+            //get word
             Scanner words = new Scanner(System.in);
 
             String word = words.nextLine();
@@ -31,58 +31,58 @@ public class A5Q1 {
             String ex = "EXIT";
             //brigadoon song
             String Brigadoon = "Brigadoon";
-            
-            
-            if(word.equals(Brigadoon)){
+
+
+            if (word.equals(Brigadoon)) {
                 //system pints goodbye and ensds
                 System.out.print("ubOh Brubigubaduboon yubour ubat schubool, wube thubink yubour vubery awubesubomube yubes wube thubink yubour cubool uboh Brubigubaduboon, ubits thube plubacube tubo bube, cubausube I rubespubect yubou uband yubou rubespubect mube, uboh Brubigubaduboon");
-                
-               break;
-                
+
+                break;
+
             }
-            
-            
-            if(word.equals(ex)){
+
+
+            if (word.equals(ex)) {
                 //system pints goodbye and ensds
                 System.out.print("goodbye");
-                
-               break;
-                
+
+                break;
+
             }
             //empty string
             String translate = "";
-            
-            Boolean foundvowel = false; 
+
+            Boolean foundvowel = false;
             //vowel counter
-            for(int i = 0; i < word.length(); i++){
-                
+            for (int i = 0; i < word.length(); i++) {
+
                 // look for a vowel at spot i 
-                if((word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u')  && foundvowel == false){
+                if ((word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u') && foundvowel == false) {
                     //add ub
-                  translate = translate + "ub";
-                  
-                  translate = translate + word.charAt(i);
-                  
-                  foundvowel = true; 
-                  
-                  
-                }else{
+                    translate = translate + "ub";
+
+                    translate = translate + word.charAt(i);
+
+                    foundvowel = true;
+
+
+                } else {
                     //skip after 2 vowels
-                    if((word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u') && foundvowel == true){
+                    if ((word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u') && foundvowel == true) {
                         translate = translate + word.charAt(i);
-                    }else{
+                    } else {
                         translate = translate + word.charAt(i);
                         foundvowel = false;
                     }
                 }
-            
-            
-    }
-            
+
+
+            }
+
             System.out.print(translate);
-            a = a +1;
-            
+            a = a + +1;
+
         }
-        
-}
+
+    }
 }
